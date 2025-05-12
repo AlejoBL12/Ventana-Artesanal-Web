587b2f55d84ae0dce2c82b91a27084d4d5c1f2aa
AlejoBL12 <barona.alejandro@correounivalle.edu.co>
2025-05-11 20:50:09 -0500

    Creacion de publicacion de producti, muestra del producto y organizacion de los codigos

--- Ventana-Artesanal/js/script.js
+++ Ventana-Artesanal/js/script.js
@@ -5,3 +5,23 @@ function previewImage(nb) {
         document.getElementById('uploadPreview'+nb).src = e.target.result;         
     };     
 }
+
+const parametros = new URLSearchParams(window.location.search);
+const nombre = parametros.get('nombre');
+const descripcion = parametros.get('descripcion');
+const categoria = parametros.get('categoria');
+const precio = parametros.get('precio');
+const imagen = parametros.get('imagen');
+
+const producto = document.getElementById('producto');
+producto.innerHTML = 
+    `<div class="productos" id="productList">
+        <article class="producto">
+            <img src="${imagen}"  />
+            <h3>${nombre}</h3>
+            <p>Categoría: ${categoria}</p>
+            <p>Precio: $${precio}</p>
+            <botto><a href="index.html">Volver</a></botto>
+        </article>
+    </div>
+    <br><br>`;
